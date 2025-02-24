f6c20f764b0d646d69c86e2a29454c2443196aa1
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-24 14:18:05 +0100

    upd app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,25 @@ from browser_detection import browser_detection_engine
 from language_detection import detect_browser_language
 
 def main():
-    st.title("Current user session:")
-    st.subheader("Cookies:")
-    st.context.cookies
+    # st.title("Current user session:")
+    # st.subheader("Cookies:")
+    # st.context.cookies
     
-    st.title("Current context headers:")    
-    st.context.headers
+    # st.title("Current context headers:")    
+    # st.context.headers
     
-    st.title("Browser detection engine:") 
-    value = browser_detection_engine()
-    st.write(value)
+    # st.title("Browser detection engine:") 
+    # value = browser_detection_engine()
+    # st.write(value)
 
-    st.title("Browser language:") 
-    browser_language = detect_browser_language()
-    # Display the detected language for debugging
-    st.write(f"Detected browser language: {browser_language}")
+    # st.title("Browser language:") 
+    # browser_language = detect_browser_language()
+    # # Display the detected language for debugging
+    # st.write(f"Detected browser language: {browser_language}")
 
 
-    st.title("Query parameters in app's URL")
-    st.query_params
+    # st.title("Query parameters in app's URL")
+    # st.query_params
 
     # Example:
     # https://your_app.streamlit.app/?first_key=1&second_key=two
@@ -31,9 +31,16 @@ def main():
     st.info(f"Product number: {st.query_params["pn"]}")
     st.info(f"Serial number: {st.query_params["sn"]}")
 
-    pn_dict = {"5F1370116301AN", "5C1440154403AN"}
-    sn_dict = {"25007676", "25007677"}
-    desc_dict = {"5F-03450-116-3-M1460-0000N", "5C-04870-154-4-M0343-0000N"}
-    
+    pn_dict = {"pn_code": ["5F1370116301AN", "5C1440154403AN"]}
+    sn_dict = {"sn_code": ["25007676", "25007677"]}
+    desc_dict = {"desc_art": ["5F-03450-116-3-M1460-0000N", "5C-04870-154-4-M0343-0000N"]}
+
+    st.selectbox(
+        label="Product number:",
+        options=pn_dict.items(),
+        index=index.pn_dict(st.query_params["pn"]),
+        disabled=False
+    )
+
 if __name__ == "__main__":
     main()
