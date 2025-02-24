9853473c31538d5743aadc2a8ef00fd997f96ecd
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-24 12:53:39 +0100

    upd app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit as st
 
 def main():
     st.title("Current user session:")
+    st.subheader("Cookies:")
     st.context.cookies
+    st.subheader("Contest header:")
     st.context.headers
 
     st.title("Query parameters in app's URL")
@@ -12,7 +14,7 @@ def main():
     # Example:
     # https://your_app.streamlit.app/?first_key=1&second_key=two
     
-    # https://iph-sn-ep33nvbuic6zbq7uqvbxbm.streamlit.app/?sn=25007677
+    # https://iph-sn-ep33nvbuic6zbq7uqvbxbm.streamlit.app/?sn=25007677&pn=5C1440154403AN
 
 if __name__ == "__main__":
     main()
