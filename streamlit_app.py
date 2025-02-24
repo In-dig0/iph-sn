adb1ab5c2cc70c501d4cb94e3f916afb661ca2b8
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-24 16:21:51 +0100

    upd app

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,16 @@ def main():
     st.info(f"Product number: {st.query_params["pn"]}", icon="ℹ️")
     st.info(f"Serial number: {st.query_params["sn"]}", icon="ℹ️")
 
-    pn_dict = {"pn_code": ["5F1370116301AN", "5C1440154403AN"]}
-    pn_list = list(pn_dict.values())
-    st.write(pn_list)
-    sn_dict = {"sn_code": ["25007676", "25007677"]}
-    desc_dict = {"desc_art": ["5F-03450-116-3-M1460-0000N", "5C-04870-154-4-M0343-0000N"]}
+    pn_list = ["5F1370116301AN", "5C1440154403AN"]
+    sn_dict = ["25007676", "25007677"]
+    desc_dict = ["5F-03450-116-3-M1460-0000N", "5C-04870-154-4-M0343-0000N"]
 
-    # st.selectbox(
-    #     label="Product number:",
-    #     options=pn_list,
-    #     index=pn_list.index(st.query_params["pn"]),
-    #     disabled=False
-    # )
+    st.selectbox(
+        label="Product number:",
+        options=pn_list,
+        index=pn_list.index(st.query_params["pn"]),
+        disabled=False
+    )
 
 if __name__ == "__main__":
     main()
