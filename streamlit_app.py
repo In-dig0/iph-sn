e7c3d11e65b516d394ead3478dbdf187e1561d45
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-24 13:00:30 +0100

    upd app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,21 @@
 import streamlit as st
-
+from browser_detection import browser_detection_engine
 
 def main():
     st.title("Current user session:")
     st.subheader("Cookies:")
     st.context.cookies
+<<<<<<< HEAD
+    
+    st.title("Current context headers:")    
+=======
     st.subheader("Contest header:")
+>>>>>>> 9853473c31538d5743aadc2a8ef00fd997f96ecd
     st.context.headers
+    
+    st.title("Browser detection engine:") 
+    value = browser_detection_engine()
+    st.write(value)
 
     st.title("Query parameters in app's URL")
     st.query_params
