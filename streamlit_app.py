d3533f80235c2a623f882cdb1ef66d385afa9ff0
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-27 10:09:35 +0100

    upd app

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,9 +113,9 @@ def main():
             disabled=False
         )
 
-        if selected_language == None or selected_attachment_type == None:       
-            st.warning("Please select language and attachment type first!", icon="⚠️")
-            st.stop()
+        # if selected_language == None or selected_attachment_type == None:       
+        #     st.warning("Please select language and attachment type first!", icon="⚠️")
+        #     st.stop()
 
         submitted = st.form_submit_button(label="Search", type="primary", icon=":material/search:")
     
